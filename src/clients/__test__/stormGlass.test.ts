0ba01dc9452dcec8a091f674a6b3933765fdc101
Roney Erdemann <roneyweb@ig.com.br>
2020-08-29 15:54:38 -0300

    Desenvolvendo API client - Normalização de dados

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -2,18 +2,20 @@ import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormglassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import { strict } from 'assert';
 
 jest.mock(`axios`);
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = -151.289824;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormglassNormalized3HoursFixture);
-  });
+  });  
 });
